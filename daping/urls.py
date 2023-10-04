7664a55b8393b35b933a02aac12cb5d1ca800662
Viana05 <133653899+Viana05@users.noreply.github.com>
2023-10-04 16:30:20 +0800

    Update urls.py post

--- daping/urls.py
+++ daping/urls.py
@@ -20,14 +20,11 @@ from . import views
 
 urlpatterns = [
     path('ranking_list', views.rank_list, name='ranking_list'),
-    path('ranking_list2', views.rank_list2, name='ranking_list2'),
-    path('ranking_list3', views.rank_list3, name='ranking_list3'),
     path('month_countall', views.month_countall, name='month_countall'),
     path('month_countavgm', views.month_countavgm, name='month_countavgm'),
     path('month_countavgy', views.month_countavgy, name='month_countavgy'),
     path('month_countly', views.month_countly, name='month_countly'),
     path('region_count', views.region_count, name='region_count'),
-    path('region_count2', views.region_count2, name='region_count2'),
     path('events_reason', views.events_reason, name='events_reason'),
     path('month_count', views.month_count, name='month_count'),
     path('getmonth', views.getmonth, name='getmonth'),
@@ -40,7 +37,7 @@ urlpatterns = [
     path('ecological_detec', views.ecological_detec, name='ecological_detec'),
     path('original_events', views.original_events, name='original_events'),
     path('risk_value', views.risk_value, name='risk_value'),
-    # path('post', views.post, name='post'),
+    path('post', views.post, name='post'),
     path('post2', views.post2, name='post2'),
     path('news_list', views.news_list , name='news_list')
 ]
